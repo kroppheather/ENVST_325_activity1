199ef428b18b734d5642a2190ff96b6998f35a03
kroppheather <kroppheather@gmail.com>
2026-01-29 16:31:38 +0000

    started in class prompts

---
+++ Activity1.R
@@ -0,0 +1,37 @@
+# in-class prompts ----
+# R does calculations
+2+10
+50*4
+
+# create a variable
+aNumber <- 324892847
+
+aNumber*5
+# adk in feet
+peaks <- c(5344,5114,4960)
+# convert to meters
+peaks/3.281
+#prominence in ft
+prom <- c(4914, 2100, 840)
+
+peaks-prom
+
+# peak names
+peakNames <- c("Mount Marcy", "Algonquin Peak", 
+               "Mount Haystack")
+
+# set up data frame
+highPeaks <- data.frame(elev = peaks,
+                        prom = prom,
+                        name = peakNames)
+
+# show the elevation column
+highPeaks$elev
+
+highPeaks[1,1]
+highPeaks[,1]
+
+# homework ----
+
+
+
